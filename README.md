46862d9f33ec3ce4824c30a8ceb9e080270e99b5
unknown <deividcuellocuevas@gmail.com>
2023-07-07 11:38:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind - ToDo List App - Vanilla JS
+
+![image](https://github.com/deividcuello/todo-list-js/assets/112868702/d7b8b95e-fd9a-4fe3-b152-46a1303647df)
--- dist/script.js
+++ dist/script.js
@@ -8,6 +8,8 @@ const input = document.querySelector('input');
 const list = document.querySelector('.list');
 const todos = document.querySelector('#todos');
 const completed = document.querySelector('#completed');
+const todoEmpty = document.querySelector('#todos span');
+const completedEmpty = document.querySelector('#completed span');
 
 
 let isEdit = false;
@@ -19,6 +21,8 @@ const deleteSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20
 const editSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="blue" class="bi bi-pen" viewBox="0 0 16 16"><path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>';
 
 const completeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>';
+
+const uncompletedSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
 
 
 form.addEventListener('submit', (e) => {
@@ -42,7 +46,27 @@ form.addEventListener('submit', (e) => {
     addTodo(value);
     value = '';
     input.value = value;
+
+    checkIsEmpty();
 })
+
+function checkIsEmpty(){
+    if(tasks.todo.length > 0){
+        todoEmpty.innerHTML = ''
+        todoEmpty.classList.add('absolute');
+    } else{
+        todoEmpty.innerHTML = 'Empty'
+        todoEmpty.classList.remove('absolute');
+    }
+    
+    if(tasks.completed.length > 0){
+        completedEmpty.innerHTML = ''
+        completedEmpty.classList.add('absolute');
+    } else{
+        completedEmpty.innerHTML = 'Empty'
+        completedEmpty.classList.remove('absolute');
+    }
+}
 
 function addTodo(text, isTodo){
     const divContainer = document.createElement('div');
@@ -95,6 +119,8 @@ function deleteFunc(){
         completed.removeChild(item)
         tasks.completed.splice(tasks.todo.indexOf(value), 1);
     }
+
+    checkIsEmpty();
 }
 
 function editContent(value){
@@ -130,22 +156,28 @@ function completeFunc(){
     const parentId = item.parentNode.id;
     if(parentId == 'todos'){
         tasks.completed.push(`${h3} ed`)
+        this.innerHTML = uncompletedSvg;
         completed.appendChild(item)
+        item.querySelector('h3').classList.add('line-through');
         tasks.todo.splice(tasks.todo.indexOf(`${h3} do`), 1);
         
     } else{
         tasks.todo.push(`${h3} do`)
         todos.appendChild(item)
+        item.querySelector('h3').classList.remove('line-through');
+        this.innerHTML = completeSvg;
         tasks.completed.splice(tasks.completed.indexOf(`${h3} ed`), 1);
     }
+
+    checkIsEmpty();
 }
 
 function renderTasks(){
-    while (todos.children.length > 1) {
+    while (todos.children.length > 2) {
         todos.removeChild(todos.lastChild);
     }
     
-    while (completed.children.length > 1) {
+    while (completed.children.length > 2) {
         completed.removeChild(completed.lastChild);
     }
     
@@ -156,4 +188,6 @@ function renderTasks(){
     tasks.completed.forEach(task => {
         addTodo(task.slice(0, -3), 'completed')
     });
+
+    checkIsEmpty();
 }
